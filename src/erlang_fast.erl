53f5210e147132148c00b979a2dcb069bce7f741
Dmitry Melnikov <dmitryme@gmail.com>
2010-09-17 10:18:22 +0400

    reset method added

--- src/erlang_fast.erl
+++ src/erlang_fast.erl
@@ -5,12 +5,17 @@
 -export
 ([
       create_context/2
+      ,reset/1
       ,decode/2
 ]).
 
 create_context(TemplatesFileName, Logger) ->
    {Dicts, Templates} = erlang_fast_xml:parse(TemplatesFileName),
    #context{dicts = Dicts, templates = Templates, logger = Logger}.
+
+reset(Context = #context{dicts = Dicts}) ->
+   Dicts1 = gb_trees:map(fun(_K, _V) -> undef end, Dicts),
+   Context#context{dicts = Dicts1}.
 
 decode(Data, Context) ->
    erlang_fast_segment:decode(Data, Context).
