16e66ee176867da2b58d56043f3d8dc5993fbfd7
Dmitry Melnikov <dmitryme@gmail.com>
2012-05-08 12:07:06 +0400

    Update src/erlang_fast_test.erl

--- src/erlang_fast_test.erl
+++ src/erlang_fast_test.erl
@@ -1,134 +1,22 @@
--module(erlang_fast_test).
-
--include("erlang_fast_template.hrl").
--include("erlang_fast_context.hrl").
-
--include_lib("eunit/include/eunit.hrl").
-
--define(xmlDescr,
-   "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
-    <templates xmlns=\"http://www.fixprotocol.org/ns/fast/td/1.1\">
-    <template dictionary=\"1\" id=\"1\" name=\"MDIncRefresh\">
-      <string id=\"1128\" name=\"ApplVerID\">
-         <constant value=\"13\"/>
-      </string>
-      <string id=\"35\" name=\"MessageType\">
-         <constant value=\"X\"/>
-      </string>
-      <string id=\"49\" name=\"SenderCompID\">
-         <constant value=\"XXXXX\"/>
-      </string>
-      <uInt32 id=\"34\" name=\"MsgSeqNum\"/>
-      <uInt64 id=\"52\" name=\"SendingTime\"/>
-      <string id=\"43\" name=\"PosDupFlag\" presence=\"optional\">
-         <default/>
-      </string>
-      <uInt32 id=\"75\" name=\"TradeDate\"/>
-      <sequence name=\"MDEntries\">
-         <length id=\"268\" name=\"NoMDEntries\"/>
-         <uInt32 id=\"279\" name=\"MDUpdateAction\">
-            <copy value=\"1\"/>
-         </uInt32>
-         <uInt32 id=\"1023\" name=\"MDPriceLevel\">
-            <increment value=\"1\"/>
-         </uInt32>
-         <string id=\"269\" name=\"MDEntryType\">
-            <copy value=\"0\"/>
-         </string>
-         <uInt32 id=\"273\" name=\"MDEntryTime\">
-            <copy/>
-         </uInt32>
-         <uInt32 id=\"22\" name=\"SecurityIDSource\">
-            <constant value=\"8\"/>
-         </uInt32>
-         <uInt32 id=\"48\" name=\"SecurityID\">
-            <copy/>
-         </uInt32>
-         <uInt32 id=\"83\" name=\"RptSeq\">
-            <increment/>
-         </uInt32>
-         <decimal id=\"270\" name=\"MDEntryPx\">
-            <exponent>
-               <default value=\"0\"/>
-            </exponent>
-            <mantissa>
-               <delta/>
-            </mantissa>
-         </decimal>
-         <int32 id=\"271\" name=\"MDEntrySize\">
-            <delta/>
-         </int32>
-         <uInt32 id=\"346\" name=\"NumberOfOrders\">
-            <delta/>
-         </uInt32>
-         <string id=\"336\" name=\"TradingSessionID\">
-            <default value=\"2\"/>
-         </string>
-      </sequence>
-   </template>
-   </templates>").
-
--define(msg, {1,
-       [{"ApplVerID",<<"13">>},
-        {"MessageType",<<"X">>},
-        {"SenderCompID",<<"XXXXX">>},
-        {"MsgSeqNum",3033666},
-        {"SendingTime",20100713060000051},
-        {"TradeDate",20100713},
-        {"MDEntries",
-         [[{"MDUpdateAction",1},
-           {"MDPriceLevel",2},
-           {"MDEntryType",<<"0">>},
-           {"MDEntryTime",60000000},
-           {"SecurityIDSource",8},
-           {"SecurityID",26714},
-           {"RptSeq",5049472},
-           {"MDEntryPx",{107425,0}},
-           {"MDEntrySize",189},
-           {"NumberOfOrders",30},
-           {"TradingSessionID",<<"2">>}],
-          [{"MDUpdateAction",1},
-           {"MDPriceLevel",3},
-           {"MDEntryType",<<"0">>},
-           {"MDEntryTime",60000000},
-           {"SecurityIDSource",8},
-           {"SecurityID",26714},
-           {"RptSeq",5049473},
-           {"MDEntryPx",{1074,2}},
-           {"MDEntrySize",246},
-           {"NumberOfOrders",34},
-           {"TradingSessionID",<<"2">>}],
-          [{"MDUpdateAction",1},
-           {"MDPriceLevel",1},
-           {"MDEntryType",<<"1">>},
-           {"MDEntryTime",60000000},
-           {"SecurityIDSource",8},
-           {"SecurityID",26714},
-           {"RptSeq",5049474},
-           {"MDEntryPx",{107475,0}},
-           {"MDEntrySize",72},
-           {"NumberOfOrders",18},
-           {"TradingSessionID",<<"2">>}]]}]}).
-
 -define(msg1, {1,
-       [{"ApplVerID",<<"13">>},
-        {"MessageType",<<"X">>},
-        {"SenderCompID",<<"XXXXX">>},
-        {"MsgSeqNum",3033667},
-        {"SendingTime",20100713060000052},
-        {"TradeDate",20100713},
-        {"MDEntries",
-         [[{"MDUpdateAction",1},
-           {"MDPriceLevel",1},
-           {"MDEntryType",<<"0">>},
-           {"MDEntryTime",60000000},
-           {"SecurityIDSource",8},
-           {"SecurityID",26714},
-           {"RptSeq",5049475},
-           {"MDEntryPx",{10745,1}},
-           {"MDEntrySize",95},
-           {"NumberOfOrders",12},
-           {"TradingSessionID",<<"2">>}]]}]}).
+       [{<<"ApplVerID">>,<<"13">>},
+        {<<"MessageType">>,<<"X">>},
+        {<<"SenderCompID">>,<<"XXXXX">>},
+        {<<"MsgSeqNum">>,3033667},
+        {<<"SendingTime">>,20100713060000052},
+        {<<"TradeDate">>,20100713},
+        {<<"MDEntries">>,
+         [[{<<"MDUpdateAction">>,1},
+           {<<"MDPriceLevel">>,1},
+           {<<"MDEntryType">>,<<"0">>},
+           {<<"MDEntryTime">>,60000000},
+           {<<"SecurityIDSource">>,8},
+           {<<"SecurityID">>,26714},
+           {<<"RptSeq">>,5049475},
+           {<<"MDEntryPx">>,{10745,1}},
+           {<<"MDEntrySize">>,95},
+           {<<"NumberOfOrders">>,12},
+           {<<"TradingSessionID">>,<<"2">>}]]}]}).
 
 -define(data, <<192, 129, 1, 57, 20, 194, 35, 90, 47, 95, 45, 49, 66, 179, 9, 74, 108, 233, 131, 174, 130, 28, 78, 14, 128,
    1, 80, 218, 2, 52, 25, 128, 6, 71, 161, 1, 189, 158, 129, 130, 121, 65, 145, 185, 132, 176, 129, 177, 6, 63, 161,
@@ -160,7 +48,7 @@ encode_test() ->
 
 find_template_test() ->
    Context = erlang_fast:create_context(?xmlDescr, fun logger/2),
-   ?assertMatch({template, "MDIncRefresh", _, 1, _, "1", _}, erlang_fast_templates:get_by_id(1,
+   ?assertMatch({template, <<"MDIncRefresh">>, _, 1, _, "1", _}, erlang_fast_templates:get_by_id(1,
          Context#context.templates#templates.tlist)).
 
 logger([], _) ->
